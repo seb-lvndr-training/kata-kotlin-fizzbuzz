6515c6e244da93745eef95281e3fcb01e5e5b894
Sebastien LAVANDIER <sebastien.lavandier@ca-ps.com>
2024-02-21 15:39:23 +0100

    Add README.md

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # kata-kotlin-fizzbuzz
-Kata - Kotlin - Fizzbuzz
+
+FizzBuzz est un jeu qui a gagné en popularité en tant que mission de programmation visant à éliminer les 
+non-programmeurs lors des entretiens d'embauche. L'objet du devoir consiste moins à le résoudre 
+correctement selon les règles ci-dessous qu'à montrer que le programmeur comprend les outils de base nécessaires 
+tels que les instructions if-/else et les boucles. 
+
+# Les règles de FizzBuzz sont les suivantes :
+
+    Pour les nombres 1 à 100,
+    
+    - si le nombre est divisible par 3, retourner Fizz ;
+    - si le nombre est divisible par 5, retourner Buzz ;
+    - si le nombre est divisible par 3 et 5 (15) retourner FizzBuzz ;
+    - sinon retourner le numéro.
--- src/main/kotlin/com/example/fizzbuzz/fizzbuzz.kt
+++ src/main/kotlin/com/example/fizzbuzz/fizzbuzz.kt
@@ -5,20 +5,12 @@ object Fizzbuzz {
     fun execute(value: Int): String {
         if (value > 0) {
             when {
-                isModulo3(value) && isModulo5(value) -> return "Fizzbuzz"
-                isModulo3(value) -> return "Fizz"
-                isModulo5(value) -> return "Buzz"
+                isModulo(value, 15) -> return "Fizzbuzz"
+                isModulo(value, 5) -> return "Buzz"
+                isModulo(value, 3) -> return "Fizz"
             }
         }
         return value.toString()
-    }
-
-    private fun isModulo3(value: Int): Boolean {
-        return isModulo(value, 3)
-    }
-
-    private fun isModulo5(value: Int): Boolean {
-        return isModulo(value, 5)
     }
 
     private fun isModulo(value: Int, module: Int): Boolean {
